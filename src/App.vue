091f1e7a7a03f921057602c124771d2ad41832c3
Marc Baldevey <marcbaldevey@inedit.com>
2023-09-26 19:32:24 +0200

    - First Commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <apexchart type="line" :options="chartOptions" :series="series" />
+</template>
+<script setup lang="ts">
+const chartOptions = {
+  chart: {
+    id: "vuechart-example",
+  },
+  xaxis: {
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+  },
+};
+const series = [
+  {
+    name: "series-1",
+    data: [30, 40, 35, 50, 49, 60, 70, 91],
+  },
+];
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
