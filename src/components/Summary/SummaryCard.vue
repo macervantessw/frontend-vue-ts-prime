352ce4a646d4e66dc6813768038343be1e8d7640
Miguel <maclyd@yahoo.es>
2025-12-07 21:51:25 +0100

    Show info

--- src/components/Summary/SummaryCard.vue
+++ src/components/Summary/SummaryCard.vue
@@ -6,10 +6,25 @@ defineProps({
   },
 });
 </script>
+
 <template>
-  <div class="summary-card p-4 shadow-1 flex flex-column text-xl font-semibold text-700 w-full sm:w-auto">
-    <h2 class="mt-0 font-bold text-3xl text-primary">{{ title }}</h2>
+  <div
+    class="summary-card p-4 shadow-1 flex flex-column text-xl font-semibold text-700 w-full sm:w-auto"
+  >
+    <h2 class="mt-0 font-bold text-3xl text-primary">
+      {{ title }}
+    </h2>
     <slot />
   </div>
 </template>
-<style></style>
+
+<style>
+.summary-card {
+  /* Necesario para que los elementos con position:absolute
+     dentro (como tu icono y tooltip) se posicionen bien */
+  position: relative;
+
+  /* Para que el tooltip no se corte si sale fuera del card */
+  overflow: visible;
+}
+</style>
